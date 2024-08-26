47548588f7a8585b08243a1378744dce7177fe87
Jason Lin <linjay@umich.edu>
2024-08-26 14:14:06 -0400

    Finished skeleton for avatars

--- plmGPT/src/components/avatars.jsx
+++ plmGPT/src/components/avatars.jsx
@@ -1,24 +1,78 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { Card } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 export default function Avatars() {
-  return (
-    <Stack direction="row" spacing={50}>
-        <Avatar 
-            alt="Temp" 
-            src="/jason_avatar.png"
-            sx={{ width: '12rem', 
-                height: '12rem',
-                transform: 'rotate(-3deg)'
-            }}
-        />
-        <Avatar 
-            alt="Temp" 
-            src="/vite.svg"
-            sx={{ width: '7rem', height: '7rem' }}
-        />
-    </Stack>
-    
-  );
+    return (
+        <Stack direction="row" spacing={15} justifyContent="center">
+            <Card sx={{ 
+                width: 300,
+                height: 500,
+                display: 'flex', 
+                alignItems: 'center',  
+                flexDirection: 'column',
+            }}>
+                <Avatar 
+                    alt="Temp" 
+                    src="/jason_avatar.png"
+                    sx={{ 
+                        width: '12rem', 
+                        height: '12rem',
+                        transform: 'rotate(-3deg)',
+                        mt: 3,
+                    }}
+                />
+                <CardContent>
+                    <Typography 
+                        gutterBottom 
+                        variant="h5" 
+                        component="div" 
+                        sx={{ textAlign: 'center' }}
+                    >
+                        Jason Lin
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+
+            <Card sx={{ 
+                width: 300,
+                height: 500,
+                display: 'flex', 
+                alignItems: 'center',  
+                flexDirection: 'column',
+            }}>
+                <Avatar 
+                    alt="Temp" 
+                    src="/jason_avatar.png"
+                    sx={{ 
+                        width: '12rem', 
+                        height: '12rem',
+                        transform: 'rotate(-3deg)',
+                        mt: 3,
+                    }}
+                />
+                <CardContent>
+                    <Typography 
+                        gutterBottom 
+                        variant="h5" 
+                        component="div" 
+                        sx={{ textAlign: 'center' }}
+                    >
+                        Thomas King
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Stack>
+    );
 }
