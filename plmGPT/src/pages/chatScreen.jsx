418231c5a2be714e8046c6dfde86758fe6f96892
Jason Lin <linjay@umich.edu>
2024-09-04 12:03:33 -0400

    added a dialog screen

--- plmGPT/src/pages/chatScreen.jsx
+++ plmGPT/src/pages/chatScreen.jsx
@@ -1,19 +1,41 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import '@styles/chatScreen.css'
+import '@styles/chatScreen.css';
 import { FaArrowUp } from "react-icons/fa6";
 import { useLocation } from 'react-router-dom';
-
+import { IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import { motion } from 'framer-motion';
+import PillButton from '../components/pillButton';
 function Chat() {
-const location = useLocation();
-  const [messages, setMessages] = useState([]);
+  const location = useLocation();
+  const [messages, setMessages] = useState(() => {
+    const storedMessages = sessionStorage.getItem('chatMessages');
+    return storedMessages ? JSON.parse(storedMessages) : [];
+  });
   const [socket, setSocket] = useState(null);
   const [currentMessage, setCurrentMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [renderedMessages, setRenderedMessages] = useState([]);
   const assistantChoice = new URLSearchParams(location.search).get('assistantChoice');
+  const [showOverlay, setShowOverlay] = useState(false);
   const chatScreenRef = useRef(null);
+  const handleOverlay = () => {
+    setShowOverlay(false);
+    sessionStorage.setItem('hasVisited', 'true');
+    setAnimate(true);
+  };
+  useEffect(() => {
+    const hasVisited = sessionStorage.getItem('hasVisited');
+    if (!hasVisited) {
+        setShowOverlay(true);
+    }
+  }, []);
+  useEffect(() => {
+    sessionStorage.setItem('chatMessages', JSON.stringify(messages));
+  }, [messages]);
+
   useEffect(() => {
     const ws = new WebSocket(`wss://backend-ckmm.onrender.com/ws/stream/${assistantChoice}/`);
     ws.onopen = () => {
@@ -48,6 +70,15 @@ const location = useLocation();
     };
   }, [assistantChoice]);
 
+  const overlayAnimation = {
+    hidden: { opacity: 0, scale: 0.9 },
+    visible: { 
+        opacity: 1, 
+        scale: 1,
+        transition: { duration: 0.5, ease: 'easeOut' }
+    },
+    exit: { opacity: 0, scale: 0.9, transition: { duration: 0.3 } }
+};
   const updateCurrentMessage = (deltaText) => {
     setCurrentMessage(prev => prev + deltaText);
   };
@@ -86,11 +117,13 @@ const location = useLocation();
     }));
     setRenderedMessages(processedMessages);
   }, [messages]);
+
   useEffect(() => {
     if (chatScreenRef.current) {
-      chatScreenRef.current.scrollTop = chatScreenRef.current.scrollHeight;
+        chatScreenRef.current.scrollTop = chatScreenRef.current.scrollHeight;
     }
   }, [renderedMessages, loading]);
+
   const [input, setInput] = useState('');
   const handleSend = () => {
     if (input.trim() !== '') {
@@ -99,12 +132,47 @@ const location = useLocation();
     }
   };
 
+  const handleClearMessages = () => {
+    setMessages([]);
+    sessionStorage.removeItem('chatMessages');
+  };
+
   return (
-    <div className="container">
-      <div className="chat">
+    <div className='container'>
+            {showOverlay && (
+                <motion.dialog 
+                    className='welcomeDialog'
+                    open 
+                    variants={overlayAnimation} 
+                    initial="hidden" 
+                    animate="visible" 
+                    exit="exit"
+                >
+                    <span style={{ fontWeight: 'bold' }}>
+                        Survey Disclaimer
+                    </span>
+                    <p>
+                        Thank you for taking the time to participate in our survey. We want to assure you that your responses will be used exclusively for general analytical purposes. No personal information that could identify you individually will be collected. Your privacy is important to us, and we are committed to maintaining the confidentiality of your responses.
+                    </p>
+                    <form method="dialog">
+                        <div className="dialogButtonContainer">
+                            <PillButton onClick={handleOverlay}>Start Survey</PillButton>
+                        </div>
+                    </form>
+                </motion.dialog>
+            )}
+      <div className={`chat ${showOverlay ? 'blurred disabled' : ''}`}>
         <div className='pageHeader'>
           <h3>TEMP</h3>
-
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="newChat"
+            onClick={handleClearMessages} 
+          >
+            <ChatIcon />
+          </IconButton>
         </div>
         <div className='chatScreen' ref={chatScreenRef}>
           {renderedMessages.map((message, index) => (
@@ -112,7 +180,6 @@ const location = useLocation();
               key={index}
               className={`messageContainer ${message.isUserMessage ? 'userMessage' : 'responseMessage'}`}
             >
-
               <div className="messageText">
                 {message.htmlContent}
               </div>
@@ -135,9 +202,9 @@ const location = useLocation();
               }}
             />
             <button 
-            className={`sendButton ${loading ? 'disabledButton' : ''}`} 
-            onClick={handleSend} 
-            disabled={loading}>
+              className={`sendButton ${loading ? 'disabledButton' : ''}`} 
+              onClick={handleSend} 
+              disabled={loading}>
               <FaArrowUp className='arrowIcon' />
             </button>
           </div>
